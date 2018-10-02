7f99e10f22d5f79af2b52802035f2c8eeb6d148e
Mary Rys <rys.mary@gmail.com>
2018-10-01 20:34:10 -0500

    added data

--- javascripts/Components/characters.js
+++ javascripts/Components/characters.js
@@ -1,1 +1,27 @@
-console.log('characters');
+const characters = [{
+    id: 'character 1', 
+    name: 'Daenerys Targaryen', 
+    house: 'Targaryen', 
+    imgURL: 'https://vignette.wikia.nocookie.net/gameofthrones/images/5/5f/Daenerys_Dragonpit.jpg/revision/latest?cb=20171015095128'
+    },
+    {
+    id: 'character 2',
+    name: 'Jon Snow',
+    house: 'Stark',
+    imgURL: 'https://vignette.wikia.nocookie.net/gameofthrones/images/a/a5/Profile-JonSnow-707.png/revision/latest?cb=20170828030553'
+    },
+    {
+    id: 'character 3',
+    name: 'Cersei Lannister',
+    house: 'Lannister',
+    imgURL: 'https://vignette.wikia.nocookie.net/gameofthrones/images/c/c3/Profile-CerseiLannister.png/revision/latest?cb=20170828071355'
+    },
+    {
+    id: 'character 4',
+    name: 'Samwell Tarly',
+    house: 'Tarly',
+    imgURL: 'https://vignette.wikia.nocookie.net/gameofthrones/images/e/e9/Sam_at_Winterfell.jpg/revision/latest?cb=20170903200558'
+}
+];
+
+    console.log('characters', characters);
